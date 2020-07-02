05f9ea4b02653f3bdac48aa5242d48ab3529ce3f
ferrisbueller66 <github email address>
2020-07-02 13:48:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+
+function findMatching(shortList, query) {
+    return shortList.filter(function(el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+  }
+
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(function(el) {
+      console.log(el.substring(0, 2))
+      return el.substring(0, 2).toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+  }
+//   function fuzzyMatch(drivers, query) {
+//     return drivers.filter(function(el) {
+//       console.log(el.charAt(0))
+//       if (query.length === 1){
+//         return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+//       }
+      
+//     })
+//   }
+
+function matchName(drivers, query){
+    return drivers.filter(it => it.name.includes(query));
+    }
