98062a79efb860e73aae4200c819e94439232648
Amanda <main@Amandas-MacBook-Air.local>
2020-07-03 22:23:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+const { match } = require("sinon")
+
+
+function findMatching(arr, str) {
+  return arr.filter(maybe => 
+    maybe.toLowerCase() === str.toLowerCase()
+  )
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter(maybe => 
+    maybe.toLowerCase().indexOf(str.toLowerCase()) === 0
+  )
+}
+
+function matchName(arr, arrName) {
+  return arr.filter(maybe => maybe.name === arrName)
+}
