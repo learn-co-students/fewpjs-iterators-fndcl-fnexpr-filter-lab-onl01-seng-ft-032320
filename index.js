4c9d92e4375422cecae70809071acf7986abf0f8
Diane Crame <dcrame@icloud.com>
2020-07-03 19:07:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string){
+    return drivers.filter(word => {
+      return word.toLowerCase() == string.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(word => {
+        return word.startsWith(string)
+    })
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driver => {
+      return driver['name'] == string
+    })
+  }
