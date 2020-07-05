0cb1e70c66a82af5f5e5d2689ab522fa63b34696
samiylo <skryshu@gmail.com>
2020-07-05 11:40:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+const { match } = require("sinon")
+
+function findMatching(drivers,name) {
+    let matching = drivers.filter(driver => {
+        return driver.toUpperCase() === name.toUpperCase()
+
+    })
+    return matching
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver =>
+        driver.toUpperCase().indexOf(name.toUpperCase())=== 0
+    )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter( record => record.name === name)
+}
